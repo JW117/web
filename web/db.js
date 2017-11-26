865efa6789ec21d3fd2af11bf320138f1f3a6131
JW117 <sleepyotter@daum.net>
2017-11-26 09:21:14 +0900

    Update db.js

--- web/db.js
+++ web/db.js
@@ -5,7 +5,7 @@ var pool;
 exports.connect = function(done) {
     pool = mysql.createPool({
         connectionLimit: 100,   //접속 인원 수. 앱 접속 인원 많아지면 증가시킬 필요
-        host : '139.59.240.46:80',
+        host : '139.59.240.46',
         user : 'root',
         password : 'bestfood',
         database : 'bestfood' 
